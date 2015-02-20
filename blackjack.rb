755f6f40e8b5191b384cb69b8fa429da414adaef
megacartridge <mdltrack@gmail.com>
2015-02-19 23:33:42 -0500

    Added 'Busted' message

--- blackjack.rb
+++ blackjack.rb
@@ -2,17 +2,24 @@ i = Random.rand(19)
 i = (i + 2)
 puts i
 if i == 21
-    puts "BLACKJACK!!!"
+   puts "BLACKJACK!!!"
 else
-puts "Draw again? (y/n)"
-input = gets.chomp
-if input == 'y'
-    i = i+ Random.rand(11)
-	puts i
-elsif input == 'n'
-	puts "Your score is..."
-    puts i
-else
-	puts "......Huh?"
-end
+   puts "Draw again? (y/n)"
+   input = gets.chomp
+   if input == 'y'
+     i = i+ Random.rand(11)
+     puts i
+   elsif input == 'n'
+   puts "Your score is..."
+   puts i
+   else
+    puts "......Huh?"
+  end
+   if i > 21
+       puts "BUSTED!"
+    elsif i == 21
+        puts "You win!"
+    else
+        puts "You stayed under 21!"
+    end
 end
